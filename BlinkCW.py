5f04563bf2a882873510c4361a7fbd9df492b3ea
Tristan Dunham <t.dunham99@gmail.com>
2019-05-29 12:02:09 -0700

    Update BlinkCW.py

--- BlinkCW.py
+++ BlinkCW.py
@@ -1,19 +1,34 @@
 import RPi.GPIO as GPIO
 from time import sleep
-from builtins import False
 
-GPIO.setmode(BCM)
+GPIO.setmode(GPIO.BCM)
 
 yellow = 6
-button = 26
+green = 17
+red = 27
+button = 19
 
-GPIO.setup(yellow, GPIO.out)
+GPIO.setup(yellow, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(red, GPIO.OUT)
 GPIO.setup(button, GPIO.IN)
 
 while True:
-    if GPIO.input(button) == False:
-        GPIO.output(yellow, True)
-    else:
-        GPIO.output(yellow, False)
+	if GPIO.input(button) == False:
+		GPIO.output(yellow, True)
+		sleep(.25)
+		GPIO.output(yellow, False)
+		sleep(.5)
+		GPIO.output(green, True)
+		sleep(.25)
+		GPIO.output(green, False)
+		sleep(.5)
+		GPIO.output(red, True)
+		sleep(.25)
+		GPIO.output(red, False)
+	elif GPIO.input(button) == True:
+		GPIO.output(yellow, False)
+		GPIO.output(green, False)
+		GPIO.output(red, False)
 
 GPIO.cleanup()
